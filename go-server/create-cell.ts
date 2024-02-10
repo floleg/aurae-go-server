74397640dc6a6564ecef9be2e1fa76f552952d86
Florent Legname <florent.legname@gmail.com>
2024-02-10 15:24:28 +0100

    Changed: move go server files to specific folder

---
+++ go-server/create-cell.ts
@@ -0,0 +1,17 @@
+import * as aurae from "../../aurae/auraescript/gen/aurae.ts";
+import * as cells from "../../aurae/auraescript/gen/cells.ts";
+
+let client = await aurae.createClient();
+let cellService = new cells.CellServiceClient(client);
+
+let allocated = await cellService.allocate(<cells.CellServiceAllocateRequest>{
+    cell: cells.Cell.fromPartial({
+        name: "go-server",
+        cpu: cells.CpuController.fromPartial({
+            weight: 2, // Percentage of CPUs
+            max: 400 * (10 ** 3), // 0.4 seconds in microseconds
+        }),
+    })
+});
+
+console.log('Allocated:', allocated)

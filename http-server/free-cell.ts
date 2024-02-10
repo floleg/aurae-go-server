8705884382cd6c85cb0f83277838407f943a5dfb
Florent Legname <florent.legname@gmail.com>
2024-02-10 21:56:40 +0100

    Added: free cell allocation

---
+++ http-server/free-cell.ts
@@ -0,0 +1,13 @@
+import * as aurae from "../../aurae/auraescript/gen/aurae.ts";
+import * as cells from "../../aurae/auraescript/gen/cells.ts";
+
+let client = await aurae.createClient();
+let cellService = new cells.CellServiceClient(client);
+
+let cellName = "http-server";
+
+// [ Free ]
+let freed = await cellService.free(<cells.CellServiceFreeRequest>{
+    cellName
+});
+console.log(freed)

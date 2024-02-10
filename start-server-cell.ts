f4ff14c7d8b36dbdfca5e9b8c1f5a0243ecdfed7
Florent Legname <florent.legname@gmail.com>
2024-02-10 15:11:15 +0100

    Added: server's cell declarations

---
+++ start-server-cell.ts
@@ -0,0 +1,16 @@
+import * as aurae from "../aurae/auraescript/gen/aurae.ts";
+import * as cells from "../aurae/auraescript/gen/cells.ts";
+
+let client = await aurae.createClient();
+let cellService = new cells.CellServiceClient(client);
+let cellName = "go-server";
+
+let started = await cellService.start(<cells.CellServiceStartRequest>{
+    cellName: "go-server",
+    executable: cells.Executable.fromPartial({
+        command: "/home/florent/devel/go-http/main",
+        description: "Run a simple http server",
+        name: cellName
+    })
+})
+console.log('Started:', started)
---
+++ stop-server-cell.ts
@@ -0,0 +1,13 @@
+import * as aurae from "../aurae/auraescript/gen/aurae.ts";
+import * as cells from "../aurae/auraescript/gen/cells.ts";
+
+let client = await aurae.createClient();
+let cellService = new cells.CellServiceClient(client);
+
+let cellName = "go-server";
+
+// [ Stop ]
+let stopped_out = await cellService.stop(<cells.CellServiceStopRequest>{
+    cellName,
+    executableName: cellName,
+})
